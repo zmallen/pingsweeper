0a3289696c1f03ffe76ad33ecfea9e7fea9057ee
Zack Allen <zma4580@gmail.com>
2017-03-03 15:16:17 -0500

    Hey, fixed that bug you introduced

--- test_ping.py
+++ test_ping.py
@@ -1,7 +1,7 @@
 import ping
 
 def test_read_file():
-    lines = ping.read_file('/Users/zallen/git2/pingsweeper/ips.txt')
+    lines = ping.read_file('./ips.txt')
     assert(lines == ['8.8.8.8', '8.8.8.4'])
 
 def test_foo():
