855a4a4d65797916963d86fded69c22357f37482
Zack Allen <zma4580@gmail.com>
2017-03-03 15:08:20 -0500

    Works locally

--- ping.py
+++ ping.py
@@ -1,3 +1,13 @@
+def read_file(ipfile):
+    with open(ipfile, 'r') as fd:
+        lines = fd.readlines()
+    ips = []
+    for line in lines:
+        ips.append(line.strip())
+    return ips
+
+def ping():
+    return ''
 
 def main():
     print 'derp'
